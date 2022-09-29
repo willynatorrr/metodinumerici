27d7569fd5ca0f1ab49d85cd891ccf04a0e26db1
william.iania <w.iania@studenti.unipi.it>
2022-09-29 17:55:36 +0200

    aggiunta file savingdata

--- savingdata.f
+++ savingdata.f
@@ -3,5 +3,12 @@ C     Il codice salva dati in un file
       implicit none
       real a, b, c
       integer i, n
-      parameter(a.eq.12.5, b.eq.15.8, c.eq.81.89, i.eq.2, n.eq.100)
-      open(unit1, FILE='risultati.ris', STATUS='old')
+C     parameter(a.eq.12.5, b.eq.15.8, c.eq.81.89, i.eq.2, n.eq.100)
+      open(10, FILE='dati.dat', STATUS='old')
+      open(11, FILE='risultati.ris', STATUS='unknown')
+      read(10,*) a, b, c, i, n
+      write(11,*) a, b, c, i, n
+      close(10)
+      close(11)
+      END
+      
