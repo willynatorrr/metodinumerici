606d206cb45eb1f85d12290d6879cd6da386a417
william.iania <w.iania@studenti.unipi.it>
2022-10-02 17:53:08 +0200

    es1

--- es1.f
+++ es1.f
@@ -1,6 +1,6 @@
 C     ALGORITMO DI METROPOLIS-HASTINGS
 C     Applicazione per la gaussiana
-C     Parte 1 - Generazione campione stocastico gaussiano
+C     Parte 1 - Generazione campione stocastico gaussiano con mu=5, sigma=1
 C     IANIA WILLIAM --- VALENTINO ALESSANDRA
 C     Metodi Numerici per la Fisica - a.a. 2022/2023
 C     Parto da x_0=pi. Genero un numero casuale nell'intervallo (x_0-delta,x-O+delta) con delta=10**(-2)
@@ -13,13 +13,18 @@ C     Dichiaro la subroutine che mi permette di fare ciò nel programma main.
       call random_number(u)
       x= (b - a ) * u + a
       end subroutine
+C     Avvio il programma che scrive nel file 'dati.dat' il campione generato
       program es1
       implicit none
       real, parameter :: pi = 3.141592, e = 2.71828, delta=1E-2
       real :: x
+      integer :: i
       x=pi
-      call random_uniform(x,x-delta,x+delta)
-      print *, x
+      open(10,file='dati.dat',status='unknown')
+      do i=0,50
+         call random_uniform(x,x-delta,x+delta)
+         write(10,*) x
+      end do
       end program
       
       
