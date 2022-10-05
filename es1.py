af889dd632e0b9063e0385eb8e641412386d71e7
william.iania <w.iania@studenti.unipi.it>
2022-10-05 09:40:29 +0200

    es1

---
+++ es1.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+import pylab
+import numpy as np
+x=np.linspace(0,1.1e5,1.1e5)
+y=np.loadtxt('data.txt', 'float')
+plt.title('')
+plt.show()
---
+++ plotting.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import pylab
+import numpy as np
+
+x=np.linspace(0,1.1e5,1.1e5)
+y=np.loadtxt('data.txt', 'float')
+plt.title('')
+plt.plot(x,y, 'red')
+plt.show()
